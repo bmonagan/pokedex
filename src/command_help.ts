97d4e6a4bf27d45ac4334fb097979a7faae3c21f
bmonagan <bernardmonagan@gmail.com>
2026-04-14 22:37:17 -0400

    refactor: update commandHelp function to accept State parameter and retrieve commands from state

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,9 +1,9 @@
-import { getCommands } from "./command_register.js";
-export function commandHelp() {
+import { State } from "./state.js";
+export function commandHelp(state: State) {
     console.log("Welcome to the Pokedex!");
     console.log("Usage:\n");
-    const commands = getCommands();
+    const commands = state.commands;
     for (const [name, cmd] of Object.entries(commands)) {
-        console.log(name, ":", cmd.description);
+        console.log(`${name}:`, cmd.description);
 }
 }
