a91197b77e80d92193b764fb543e520df2ce6efa
bmonagan <bernardmonagan@gmail.com>
2026-04-15 15:05:31 -0400

    feat: implement fetchLocations method to retrieve location data from PokeAPI

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,10 +1,21 @@
+import { json } from "node:stream/consumers";
+
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
+  private static readonly baseLocationURL = "https://pokeapi.co/api/v2/location-area/"
 
   constructor() {}
 
   async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
-    // implement this
+    const url = pageURL ?? PokeAPI.baseLocationURL;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch locations: ${response.status} ${response.statusText}`);
+    }
+
+    const locations: ShallowLocations = await response.json();
+    return locations;
   }
 
   async fetchLocation(locationName: string): Promise<Location> {
@@ -13,7 +24,11 @@ export class PokeAPI {
 }
 
 export type ShallowLocations = {
-  // add properties here
+  count: number
+  next: null | string
+  previous: null | string
+  results: location[]
+
 };
 
 export type Location = {
