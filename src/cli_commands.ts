95bb5fe289449de422d29be0a6da0219220cc9d8
bmonagan <bernardmonagan@gmail.com>
2026-04-14 21:42:49 -0400

    feat: add help command to CLI for displaying help messages

--- src/cli_commands.ts
+++ src/cli_commands.ts
@@ -1,4 +1,5 @@
 import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
 
 export type CLICommand = {
   name: string;
@@ -12,6 +13,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "exit",
       description: "Exits the pokedex",
       callback: commandExit,
+    },
+    help: {
+      name: "help",
+      description : "Displays a help message",
+      callback: commandHelp,
     },
     // can add more commands here
   };
