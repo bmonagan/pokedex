743c61d71919939360da5ad5ee69be8cd347eaac
bmonagan <bernardmonagan@gmail.com>
2026-04-15 15:43:55 -0400

    feat: make commandHelp function asynchronous to enhance command processing

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,5 +1,5 @@
 import { State } from "./state.js";
-export function commandHelp(state: State) {
+export async function commandHelp(state: State) {
     console.log("Welcome to the Pokedex!");
     console.log("Usage:\n");
     const commands = state.commands;
