a0c8578ca344c2e28588524a5ea6a276026ba054
bmonagan <bernardmonagan@gmail.com>
2026-04-14 21:43:46 -0400

    refactor: was creating two newlines using the previous way now is only one.

--- src/command_help.ts
+++ src/command_help.ts
@@ -1,8 +1,7 @@
 import { getCommands } from "./cli_commands.js";
 export function commandHelp() {
     console.log("Welcome to the Pokedex");
-    console.log("Usage:");
-    console.log("\n");
+    console.log("Usage:\n");
     const commands = getCommands();
     for (const [name, cmd] of Object.entries(commands)) {
         console.log(name, ":", cmd.description);
