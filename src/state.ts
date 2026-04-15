5ec8f040da940f35da777aaceda4b566f1793688
bmonagan <bernardmonagan@gmail.com>
2026-04-14 22:04:14 -0400

    feat: add CLICommand and State types for command handling

---
+++ src/state.ts
@@ -0,0 +1,13 @@
+import { createInterface, type Interface } from "readline";
+
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (state: State) => void;
+};
+
+export type State = {
+    readline: Interface;
+    commands: Record <string, CLICommand>;
+
+    }
