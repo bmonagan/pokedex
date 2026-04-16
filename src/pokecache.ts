d919552b945b0a93e4bde8b11a67e7bc89c4d4b8
bmonagan <bernardmonagan@gmail.com>
2026-04-15 21:16:28 -0400

    feat: implement Cache class with add method for caching entries

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -2,3 +2,16 @@ export type CacheEntry<T> = {
   createdAt: number;
   val: T;
 };
+
+export class Cache {
+  #cache = new Map<string, CacheEntry<any>>();
+
+  constructor() {}
+
+  add<T>(key: string, val: T) {
+    this.#cache.set(key, {
+      createdAt: Date.now(),
+      val: val,
+    });
+  }
+}
