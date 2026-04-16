80c61314760feccdc008ca77e431a4ca5b19087c
bmonagan <bernardmonagan@gmail.com>
2026-04-16 16:29:29 -0400

    feat: implement commandCatch function for catching Pokémon

---
+++ src/command_catch.ts
@@ -0,0 +1,24 @@
+import { State } from "./state.js";
+export async function commandCatch(state: State, pokemonName: string): Promise<void> {
+    const response = await fetch(state.pokeapi.pokemonEndpointURL + pokemonName);
+    if (!response.ok) {
+        console.log("Pokemon not found..");
+    }
+    else {
+        const pokemon = await response.json();
+        console.log(`Throwing a Pokeball at ${pokemonName}`);
+        if (await isCaught(pokemon.base_experience)) { 
+            console.log(`${pokemonName} was caught!`);
+        }
+        else {
+            console.log(`${pokemonName} escaped!`);
+        }
+
+    }
+}
+
+async function isCaught(base_experience: number): Promise<boolean> {
+    const catch_chance = base_experience/300;
+    const user_roll = Math.random();
+    return user_roll > catch_chance;
+}
