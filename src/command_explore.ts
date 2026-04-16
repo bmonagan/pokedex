678fe580dc0add25b7beda29db462e46a114297e
bmonagan <bernardmonagan@gmail.com>
2026-04-16 13:57:03 -0400

    feat: refactor commandExplore to use string for areaName and enhance location handling

--- src/command_explore.ts
+++ src/command_explore.ts
@@ -1,5 +1,14 @@
 import { State } from "./state.js";
-import { Location } from "./pokeapi.js";    
-export async function commandExplore(state: State, areaName: Location ) {
-    
+import { Location, Location_endpoint, Pokemon } from "./pokeapi.js";    
+export async function commandExplore(state: State, areaName: string) {
+    const location: Location_endpoint = await state.pokeapi.fetchLocation(areaName);
+    if (location.pokemon_encounters.length < 1){
+        console.log("No pokemon were found in this area.");
+    }
+    else {
+        const names = location.pokemon_encounters.map((e) => e.pokemon.name);
+        for (const name in names) {
+            console.log(name);
+        }
+    }
 }
